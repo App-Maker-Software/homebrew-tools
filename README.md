585553ec082ed76f28121eb9da35d54c389dbdc1
Joe Hinkle <joehinkle11@gmail.com>
2021-04-04 15:18:48 -0600

    live app 0.1

---
+++ README.md
@@ -0,0 +1,13 @@
+# App Maker Software Homebrew Tools
+
+This repo hosts Homebrew rb scripts for installing:
+
+- [`liveapp`](https://github.com/App-Maker-Software/liveapp), a CLI for creating live app data and live app bundles. Primarily used to integrate into Xcode. More information at https://docs.liveapp.cc
+
+## Installing
+
+```
+# (optional) brew update
+brew install App-Maker-Software/tools/liveapp
+```
+
---
+++ liveapp.rb
@@ -0,0 +1,17 @@
+class LiveApp < Formula
+  desc "a CLI for creating live app data and live app bundles. Primarily used to integrate into Xcode. More information at https://docs.liveapp.cc"
+  homepage "https://docs.liveapp.cc"
+  url "https://github.com/App-Maker-Software/liveapp/releases/download/0.1/liveapp-0.1.tar.gz"
+  sha256 "319233d258d79400efcf149953279737b667582bdc19aa2bc3ca89fd2172467b"
+
+  bottle :unneeded
+
+  def install
+    bin.install "liveapp"
+    bin.install "lib_InternalSwiftSyntaxParser.dylib"
+  end
+
+  test do
+    system "#{bin}/liveapp", "--version"
+  end
+end
