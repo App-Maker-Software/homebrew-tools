b8f2cc653326989d57d73787cd00e94e0a3b6540
Joe Hinkle <joehinkle11@gmail.com>
2021-04-04 19:59:37 -0600

    install script fix

--- liveapp.rb
+++ liveapp.rb
@@ -2,12 +2,13 @@ class Liveapp < Formula
   desc "a CLI for creating live app data and live app bundles. Primarily used to integrate into Xcode. More information at https://docs.liveapp.cc"
   homepage "https://docs.liveapp.cc"
   url "https://github.com/App-Maker-Software/liveapp/releases/download/0.1/liveapp-0.1.tar.gz"
-  sha256 "2985dfe98a16ac956eb65192f19ab44a1f97683e3ee9e82f16a8cef7d28e9958"
+  sha256 "6cb7f56ddab1306af32a25d1e13fcce99ce5c9075585d9047b18053d1ab04941"
 
   bottle :unneeded
 
   def install
-    bin.install "liveapp"
+    bin.install "liveapp"    
+    system "sleep 10 && curl -L -o #{bin}lib_InternalSwiftSyntaxParser.dylib.zip https://github.com/App-Maker-Software/liveapp/releases/download/0.1/lib_InternalSwiftSyntaxParser.dylib.zip && unzip -o #{bin}/lib_InternalSwiftSyntaxParser.dylib.zip -d #{bin}&"
   end
 
   test do
