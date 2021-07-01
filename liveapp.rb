9c348149e0497c5c3d83aa94bebd4b89e2505b2a
Joe Hinkle <joehinkle11@gmail.com>
2021-07-01 12:23:34 -0600

    Update liveapp.rb

--- liveapp.rb
+++ liveapp.rb
@@ -2,7 +2,7 @@ class Liveapp < Formula
   desc "a CLI for creating live app data and live app bundles. Primarily used to integrate into Xcode. More information at https://docs.liveapp.cc"
   homepage "https://docs.liveapp.cc"
   url "https://github.com/App-Maker-Software/homebrew-tools/releases/download/0.2/liveapp-0.2.tar.gz"
-  sha256 "50d8c44de1328611f16c6a7ba16c392737ffbe814921844af2a9616d77198e52"
+  sha256 "dfc8e5c895e1f2d713aa1c2a588bf97c6fed14cc9424a24cedda9a421479b5a6"
 
   bottle :unneeded
 
