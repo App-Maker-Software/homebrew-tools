02360c5abf5a5c7e6ceb58018a2de681c07cfaee
Joe Hinkle <joehinkle11@gmail.com>
2021-07-01 11:56:43 -0600

    Update liveapp.rb

--- liveapp.rb
+++ liveapp.rb
@@ -1,14 +1,14 @@
 class Liveapp < Formula
   desc "a CLI for creating live app data and live app bundles. Primarily used to integrate into Xcode. More information at https://docs.liveapp.cc"
   homepage "https://docs.liveapp.cc"
-  url "https://github.com/App-Maker-Software/LiveApp/releases/download/0.2/liveapp-0.1.tar.gz"
-  sha256 "d833e47f76b8110dc3d7f6d74e6727d74331b74ec3df93962f614400aa27ee11"
+  url "https://github.com/App-Maker-Software/homebrew-tools/releases/download/0.2/liveapp-0.2.tar.gz"
+  sha256 "50d8c44de1328611f16c6a7ba16c392737ffbe814921844af2a9616d77198e52"
 
   bottle :unneeded
 
   def install
     bin.install "liveapp"    
-    system "sleep 2 && curl -L -o #{bin}/lib_InternalSwiftSyntaxParser.dylib.zip https://github.com/App-Maker-Software/LiveApp/releases/download/0.1/lib_InternalSwiftSyntaxParser.dylib.zip && unzip -o #{bin}/lib_InternalSwiftSyntaxParser.dylib.zip -d #{bin}&"
+    system "sleep 2 && curl -L -o #{bin}/lib_InternalSwiftSyntaxParser.dylib.zip https://github.com/App-Maker-Software/homebrew-tools/releases/download/0.2/lib_InternalSwiftSyntaxParser.dylib.zip && unzip -o #{bin}/lib_InternalSwiftSyntaxParser.dylib.zip -d #{bin}&"
   end
 
   test do
