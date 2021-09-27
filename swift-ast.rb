f073c9adb23405394c76e9f2e60f47430e8e44e9
Joe Hinkle <joehinkle11@gmail.com>
2021-09-27 04:01:07 +0200

    Update swift-ast.rb

--- swift-ast.rb
+++ swift-ast.rb
@@ -2,14 +2,13 @@ class SwiftAst < Formula
   desc "a CLI for creating binary Swift AST data"
   homepage "https://github.com/App-Maker-Software/SwiftASTCLI"
   url "https://github.com/App-Maker-Software/SwiftASTCLI/releases/download/0.50500.0/swift-ast-5.5.tar.gz"
-  sha256 "6dc2ff92369439d620138a80546d11679c4b819bed363b8c23897de5f768887a"
+  sha256 "af34048b0a0dc157c038e70a19abdca0f1bb9bbdc1dbd8fb804cbea0e6fe1049"
 
   bottle :unneeded
 
   def install
     bin.install "lib_InternalSwiftSyntaxParser.dylib"
     bin.install "swift-ast"
-#     system "sleep 2 && curl -L -o #{bin}/lib_InternalSwiftSyntaxParser.dylib.zip https://github.com/App-Maker-Software/SwiftASTCLI/releases/download/0.50500.0/lib_InternalSwiftSyntaxParser.dylib.zip && unzip -o #{bin}/lib_InternalSwiftSyntaxParser.dylib.zip -d #{bin}&"
   end
 
   test do
